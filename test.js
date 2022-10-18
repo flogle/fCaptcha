6bb953b6402abdde9a0930c1e4e8014921e3ecef
aixoio <aixoio@yahoo.com>
2022-10-18 16:28:04 +1100

    Make test

--- test.js
+++ test.js
@@ -3,7 +3,9 @@ $(window).on("load", function () {
     
     textfCaptcha(".fCaptcha", () => {
 
+        $(".fCaptcha").remove()
         alert("You are not a bot!")
+        $("body").append($("<h1></h1>").text("You are not a bot!"))
 
     })
 
